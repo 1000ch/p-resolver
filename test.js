1bf76843c6d98c94e103f047a294c633f1ae3adb
1000ch <shogo.sensui@gmail.com>
2018-12-15 14:59:21 +0900

    Fix test

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ test('PromiseResolver class', t => {
   t.is(typeof PromiseResolver, 'function');
 
   const resolver = new PromiseResolver(Promise.resolve('test'));
-  t.is(typeof resolver.promise.toString(), '[object Promise]');
+  t.is(resolver.promise.toString(), '[object Promise]');
   t.is(typeof resolver.resolve, 'function');
   t.is(typeof resolver.reject, 'function');
 });
